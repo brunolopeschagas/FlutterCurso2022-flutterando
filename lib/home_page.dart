9c99a6611d4f01f878f1808be5bedcc1ed556188
Bruno Chagas <brunolopeschagas@gmail.com>
2023-04-17 15:06:34 -0300

    Flutter Curso 2022 #13 - O StatefulWidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
-class HomePage extends StatelessWidget {
+//desenha na tela o widget (sem estado, deixa o estado para o State abaixo)
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+//gerencia o estado do widget
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  //chamado ao iniciar o widget
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  //chamado ao finalizar o widget
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BLSoft'),
+        title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text(
-          'BLSoft',
-        ),
+      body: Center(
+        child: Text('Home $counter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          print('alou');
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
